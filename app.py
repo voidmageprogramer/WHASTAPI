1734e2466b47cd4aa68c382ccf57f3918bd1c867
voidmage <voidmageprogramer@gmail.com>
2025-05-03 00:57:06 -0600

    SQLALCHEMY

--- app.py
+++ app.py
@@ -1,10 +1,58 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+import json
 
 app = Flask(__name__)
 
+#Configuracion  de base de datos de sqlite
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///metapython.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+#Modelo de la tabla log
+class Log(db.Model):
+    id = db.Column(db.Integer,primary_key=True) 
+    fecha_y_hora = db.Column(db.DateTime, default = datetime.utcnow)
+    texto = db.Column(db.TEXT)
+
+#Crear la tabla si nop existe
+with app.app_context():
+    db.create_all()
+
+    prueba1 = Log(texto="mensaje de prueba 1")
+    prueba2 = Log(texto="mensaje de prueba 2")
+    db.session.add(prueba1)
+    db.session.add(prueba2)
+    db.session.commit()
+
+
+
 @app.route('/')
-def hola_mundo():
-    return render_template("holaflask.html")
+def index():
+    #obtener todos los registros de l;a base de datos
+    registros = Log.query.all()
+    registros_ordenados = ordenar_por_fecha_y_hora(registros)
+    return render_template('index.html',registros=registros_ordenados)
+
+mensajes_log = []
+
+
+#Funcion para ordenar los registros por fecha y hora
+def ordenar_por_fecha_y_hora(registros):
+    return sorted(registros, key=lambda x: x.fecha_y_hora,reverse=True)
+
+# Funcion para agregar mensages y guardar en la base de datos
+
+def agregar_mensages_log(texto):
+    mensajes_log.append(texto)
+
+    #Guardar el mensaje en la base de datos
+    nuevo_registro = Log(texto = texto)
+    db.session.add(nuevo_registro)
+    db.session.commit()
+
+#agregar_mensages_log(json.dumps ("Test1"))
 
 
 if __name__=="__main__":
